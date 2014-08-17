d0b2ddb0944ae259b7a59ecd3a73df0f66de5701
byronhulcher <byronhulcher@gmail.com>
2014-08-17 13:07:23 -0400

    Info Modal + Github link

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,8 @@
+angular.module('byronhulcher.Youtubr')
+
+  .controller('NavCtrl', function($scope) {
+    $scope.modal = {
+      "title": "Youtubr",
+      "content": "Youtubr<br><a href='://youtube.xyz'>youtubr.xyz</a><br />by <a href='://github.com/byronhulcher/''>byronhulcher</a><br>Source code on <a href='://github.com/byronhulcher/youtubr'>github</a><br>2014"
+    };
+  });
