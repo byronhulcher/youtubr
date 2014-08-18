983a13e8288535b58bbfb2d2c206b82acbc82b6d
byronhulcher <byronhulcher@gmail.com>
2014-08-18 00:58:43 -0400

    Dist clean-up

--- app/scripts/filters/filters.js
+++ app/scripts/filters/filters.js
@@ -2,18 +2,6 @@
 
 angular.module('byronhulcher.Youtubr')
 
-  // .filter('time', function() {
-  //   return function(obj) {
-  //     return +new Date(obj);
-  //   };
-  // })
-
-  // .filter('startFrom', function() {
-  //   return function(obj, index) {
-  //     return obj && obj.slice(index);
-  //   };
-  // })
-
   .filter('secondsToMinutes', function(){
     return function(seconds){
       var sec = (seconds % 60).toString()
